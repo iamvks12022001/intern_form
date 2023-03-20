54a4ed648d9b2e5ef0cfaf2325f8d1a730c703d7
vikash <vikashkumar754189@gmail.com>
2023-03-20 21:01:01 +0530

    add server file

---
+++ frontend/backend/server.ts
@@ -0,0 +1,44 @@
+import express, { json } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import path from "path";
+import config from "./config.js";
+import ContactRouter from "./routers/ContactRouter.js";
+
+const port = config.PORT;
+const app = express();
+
+const errorHandler: express.ErrorRequestHandler = (
+  err: any,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const status = err.name && err.message === "ValidationError" ? 400 : 500;
+  res.status(status).send({ message: err.message });
+};
+const sendSPA: express.Handler = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  res.send(path.join(__dirname, "public", "index.html"));
+};
+
+mongoose
+  .connect(config.MONGODB_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log(error));
+
+app.use(cors());
+app.use(json());
+
+app.use("/api/contact", ContactRouter);
+
+app.use(express.static(path.join(__dirname, "public")), json());
+
+app.get("*", sendSPA);
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`serve at http://localhost:${port}`));
